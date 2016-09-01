7b2a2e4c091e14632ee2e731a841a22b7bb046b3
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-09-02 01:23:18 +0900

    add final register

--- onoffmix_auto_register.py
+++ onoffmix_auto_register.py
@@ -1,19 +1,47 @@
+import time
 from selenium import webdriver
 
-email = "testtesttest"
-pw = "testtesttest"
+email = ""
+pw = ""
 
+# onoffmix, facebook
+login_type = "facebook"
+
+meetup_url = "http://onoffmix.com/event/76453"
 
 browser = webdriver.Firefox()
 browser.get("http://www.onoffmix.com/account/login")
 
-emailBox = browser.find_element_by_id("email")
-emailBox.send_keys(email)
+if login_type == "onoffmix":
+    email_box = browser.find_element_by_id("email")
+    email_box.send_keys(email)
 
-pwBox = browser.find_element_by_id("pw")
-pwBox.send_keys(pw)
+    pw_box = browser.find_element_by_id("pw")
+    pw_box.send_keys(pw)
 
-loginBtn = browser.find_element_by_class_name("buttonLogin")
-loginBtn.click()
+    login_btn = browser.find_element_by_class_name("buttonLogin")
+    login_btn.click()
+elif login_type == "facebook":
+    time.sleep(1)
+    facebookBtn = browser.find_element_by_css_selector("li.facebook > a")
+    facebookBtn.click()
 
+    email_box = browser.find_element_by_name("email")
+    email_box.send_keys(email)
+
+    pw_box = browser.find_element_by_name("pass")
+    pw_box.send_keys(pw)
+
+    login_btn = browser.find_element_by_id("loginbutton")
+    login_btn.click()
+
+time.sleep(0.3)
+browser.get(meetup_url)
+time.sleep(0.3)
+
+register_btn = browser.find_element_by_class_name("ofmBtn")
+register_btn.click()
+
+final_register_btn = browser.find_element_by_class_name("ofmBtn")
+final_register_btn.click()
 
